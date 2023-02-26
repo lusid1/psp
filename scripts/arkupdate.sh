eef7f5977a88ef58d17a532bb7cfc750b55e3420
lusid1 <sean_hatfield@hotmail.com>
2023-02-25 23:24:25 -0800

    Update arkupdate.sh

--- scripts/arkupdate.sh
+++ scripts/arkupdate.sh
@@ -6,7 +6,6 @@ mv ARK4.zip ARK4.zip.old
 wget https://github.com/PSP-Archive/ARK-4/releases/latest/download/ARK4.zip
 if ! (cmp ARK4.zip ARK4.zip.old); then 
   rm -rf ark4
-  rm ARK4.zip.1
   rm -rf PSP
   rm data.zip
   unzip ARK4.zip -d ark4
@@ -15,7 +14,6 @@ if ! (cmp ARK4.zip ARK4.zip.old); then
   cp ark4/ARK_Live/* PSP/GAME/ARK_Live
   cp ark4/ARK_01234/* PSP/SAVEDATA/ARK_01234
   zip -r data.zip PSP
-  git clone https://github.com/lusid1/psp
   cp data.zip psp/cfw/ark4-latest/
   cp ark4/AdvancedVSH/VSHMENU.PRX psp/cfw/ark4-latest/
   cd psp
