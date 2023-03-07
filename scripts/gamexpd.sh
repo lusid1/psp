8036992cb91e464d24eeed737f0a7b1990152d12
lusid1 <sean_hatfield@hotmail.com>
2023-03-07 13:01:25 -0800

    more demos

--- scripts/gamexpd.sh
+++ scripts/gamexpd.sh
@@ -6,13 +6,6 @@
 #xpd files will be placed in that directory
 #current supports up to 8 files per game directory
 #
-#Translating eboot descriptions
-#place a file named descriptions.txt in directory from which the tool is run
-#containing a list of game codes and descriptions:
-# UCJS90019=English Title Of Game
-#
-
-
 
 #urlbase is the http path to the folder containing the game folders
 urlbase="http://archive.org/download/PSPDemoArchive"
@@ -21,19 +14,24 @@ urlbase="http://archive.org/download/PSPDemoArchive"
 xpdbase="http://archive.org/download/PSPDemoArchive/xpd"
 
 curr=$PWD
+titledb="$(dirname -- $( readlink -f -- $0; );)/titledb.txt"
+
 cd "$1" &> /dev/null
 
 dirname=$(basename $PWD)
-eid="gdp#"
 FName=$(ls -1 | grep "EBOOT.PBP" )
 if [ "$FName" == "EBOOT.PBP" ]; then
-  if [ "$(strings $FName | head -n20 | sed -n "1p")" == "APP_VER" ]; then eid="adp#";fi
-  code=$(strings EBOOT.PBP | head -n20 | sed '1,/^TITLE$/d' | sed -n "1p")
+  #Get code from eboot
+  code=$(strings -n9 EBOOT.PBP | sed -n '/^[A-Z][A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][0-9]/p' | sed -n "1p")
+  
+  #look for title in the most common places
+  code2=$(strings EBOOT.PBP | head -n20 | sed '1,/^TITLE$/d' | sed -n "1p")
   desc=$(strings EBOOT.PBP | head -n20 | sed '1,/^TITLE$/d' | sed -n "4p")
-  if [ "$code" == "USE_USB" ]; then
-    code=$(strings EBOOT.PBP | head -n20 | sed '1,/^TITLE$/d' | sed -n "3p")
+  if [ "$code2" == "USE_USB" ]; then
     desc=$(strings EBOOT.PBP | head -n20 | sed '1,/^TITLE$/d' | sed -n "6p")
   fi
+
+  #Collect file names
   AName=$( ls -p -1 | sed '/\//d' | sed '/EBOOT.PBP/d' | sed '/.[xX][pP][dD]/d' | sed '/.[pP][nN][gG]/d' |sed -n "1p") 
   FName2=$(ls -p -1 | sed '/\//d' | sed '/EBOOT.PBP/d' | sed '/.[xX][pP][dD]/d' | sed '/.[pP][nN][gG]/d' |sed -n "2p")
   AName2=$(ls -p -1 | sed '/\//d' | sed '/EBOOT.PBP/d' | sed '/.[xX][pP][dD]/d' | sed '/.[pP][nN][gG]/d' |sed -n "3p")
@@ -54,16 +52,31 @@ else #No EBOOT.PBP found but generate xpd anyway - patch or dlc
   AName4=$(ls -p -1 | sed '/\//d' | sed '/EBOOT.PBP/d' | sed '/.[xX][pP][dD]/d' | sed '/.[pP][nN][gG]/d' |sed -n "8p")
 fi
 
+eid="gdp#"
+#These install as an app - EID=adp#
+if [ "$(echo $code | grep NPIA)" != "" ]; then eid="adp#";fi   # Sensme/ Music Unlimited
+if [ "$(echo $code | grep NPEG00012)" != "" ]; then eid="adp#";fi # Digital Comics
+if [ "$(echo $code | grep NPJW00001)" != "" ]; then eid="adp#";fi # Comics Reader (jp)
+if [ "$(echo $code | grep NPJH00067)" != "" ]; then eid="adp#";fi # X-Radar (jp)
+if [ "$(echo $code | grep ULES00856)" != "" ]; then eid="adp#";fi # Go Messenger
+if [ "$(echo $code | grep NPHW00011)" != "" ]; then eid="adp#";fi # DVB-T Tuner App
+
 
 if [ "X$FName" != "X" ]; then Fsize=$(ls -l $FName | tr -s ' ' | cut -d' ' -f5); else Fsize=0;fi
 if [ "X$AName" != "X" ]; then Asize=$(ls -l $AName | tr -s ' ' | cut -d' ' -f5); else Asize=0;fi
 size=$(($Fsize+$Asize))
 size=$(($size/1024))
 
+# Done information gathering so back to the old $PWD
 cd "$curr"
 
 #If CODE is found in descriptions.txt override the sfo description with the translated one
-desceng=$(cat descriptions.txt | grep "$code" | cut -d'=' -f2- | sed -n "1p"| tr -d '\n' | tr -d '\r')
+desceng=$(cat "$titledb" | grep "$code" | cut -d' ' -f2- | sed -n "1p"| tr -d '\n' | tr -d '\r')
+if [ "X$desceng" != "X" ]; then desc="$desceng"; fi
+
+#If dirname is found in descriptions.txt override the sfo description with the translated one
+#This is for when multiple demos were released under the same content ID
+desceng=$(cat "$titledb" | grep "$dirname" | cut -d' ' -f2- | sed -n "1p"| tr -d '\n' | tr -d '\r')
 if [ "X$desceng" != "X" ]; then desc="$desceng"; fi
 
 y=1
@@ -122,6 +135,8 @@ echo "C=$urlbase/$dirname/$FName" >> "$xpd"
 if [ "X$AName" != "X" ]; then echo "A=$urlbase/$dirname/$AName" >> "$xpd";fi
 echo >> "$xpd"
 
+if [ "X$FName3" == "X" ]; then exit; fi
+
 #make part 3
 xpd="$xpd2"
 xofy="(3 of $y)"
@@ -146,6 +161,8 @@ echo "[File]" >> "$xpd"
 echo "C=$urlbase/$dirname/$FName" >> "$xpd"
 if [ "X$AName" != "X" ]; then echo "A=$urlbase/$dirname/$AName" >> "$xpd";fi
 echo >> "$xpd"
+
+if [ "X$FName4" == "X" ]; then exit; fi
 
 #make part 4
 xpd="$xpd2"
