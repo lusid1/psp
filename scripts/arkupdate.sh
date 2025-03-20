fb2287197597b755cb67e2f5031bee0d0c03752c
lusid1 <lusid@lusid.net>
2025-03-19 17:07:30 -0700

    CFW update

--- scripts/arkupdate.sh
+++ scripts/arkupdate.sh
@@ -14,10 +14,12 @@ if ! (cmp ARK4.zip ARK4.zip.old); then
   mkdir -p PSP/SAVEDATA/ARK_01234
   mkdir -p PSP/GAME/ARK_Full_Installer
   mkdir -p PSP/GAME/ARK_cIPL
+  mkdir -p PSP/GAME/ARK_DC
   cp ark4/ARK_Loader/* PSP/GAME/ARK_Loader
   cp ark4/ARK_01234/* PSP/SAVEDATA/ARK_01234
   cp ark4/PSP/ARK_Full_Installer/* PSP/GAME/ARK_Full_Installer
   cp ark4/PSP/ARK_cIPL/* PSP/GAME/ARK_cIPL
+  cp ark4/PSP/ARK_DC/* PSP/GAME/ARK_DC
   zip -r data.zip PSP
   cp data.zip psp/cfw/ark4-latest/
   cp ark4/PSP/ARK_cIPL/* psp/cfw/ark_cipl/
@@ -26,10 +28,10 @@ if ! (cmp ARK4.zip ARK4.zip.old); then
   cd psp
     git pull
     git add cfw/ark4-latest/data.zip
-    git add cfw/ark_cipl/EBOOT.PBP
-    git add cfw/ark_cipl/ipl_update.prx
-    git add cfw/ark_cipl/kbooti_update.prx
-    git add cfw/ark_cipl/kpspident.prx
+    # git add cfw/ark_cipl/EBOOT.PBP
+    # git add cfw/ark_cipl/ipl_update.prx
+    # git add cfw/ark_cipl/kbooti_update.prx
+    # git add cfw/ark_cipl/kpspident.prx
     git add cfw/infinity-2.0.5/pspgo/EBOOT.PBP
     git add cfw/infinity-2.0.5/standard/EBOOT.PBP
     git commit -m "Ark4 update automation"
